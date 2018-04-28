a999da38fb5cc57cb4f977d7fc13de98fab4b2eb
DESKTOP-KSIKVT9\SuperDamien <damien.hanns@gmail.com>
2018-04-28 10:16:51 +0100

    Check if target active finished 28/04/18

---
+++ Assets/Scripts/FSM/IsTargetActiveDecision.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "PlugableAI/Decisions/IsTargetActive")]
+public class IsTargetActiveDecision : Decision {
+
+    public override bool Decide(StateController controller)
+    {
+        bool bIsTargetActive;
+
+        if (controller.mainTarget == null) { bIsTargetActive = false; }
+        else { bIsTargetActive = controller.mainTarget.gameObject.activeSelf; }
+
+        return bIsTargetActive;
+    }
+}
---
+++ Assets/Scripts/FSM/ShootAtTargetAction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "PlugableAI/Actions/ShootAtTarget")]
+public class ShootAtTargetAction : AIAction
+{
+    public override void Act(StateController controller)
+    {
+        ShootAtTarget(controller);
+    }
+
+    void ShootAtTarget(StateController controller)
+    {
+        //shoot at mainTarget in controller
+    }
+
+}
